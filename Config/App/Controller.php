8a786275034a7af70552a3fae75fddd7b146988a
Jose Luis Pintado Vasquez <josepintado24@gmail.com>
2022-11-28 02:09:49 -0500

    Creación de vistas | Home primera parte

--- Config/App/Controller.php
+++ Config/App/Controller.php
@@ -1,7 +1,18 @@
 <?php
 class Controller
 {
-    public function cargarModelo(){
-        $model=get_class($this)."Model";
+    public function __construct()
+    {
+            $this->view=new Views();
+            $this->cargarModel();
+    }
+    public function cargarModel()
+    {
+        $model = get_class($this) . "Model";
+        $ruta = "Models/$model.php";
+        if (file_exists($ruta)) {
+            require_once $ruta;
+            $this->model = new $model();
+        }
     }
 }
